651682df0fd83a1239106dc0bd79ddf5ce57e55e
George Spiridakis <spiridakis.grg@gmail.com>
2016-08-20 18:47:26 +0300

    Populate RecyclerView programmatically in MainActivity.

--- ScrollEventsDemo/app/src/main/java/com/codeburrow/scrolleventsdemo/MainActivity.java
+++ ScrollEventsDemo/app/src/main/java/com/codeburrow/scrolleventsdemo/MainActivity.java
@@ -1,13 +1,53 @@
 package com.codeburrow.scrolleventsdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ((CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar_layout)).setTitle("Screen Title");
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new RecyclerView.Adapter<ViewHolder>() {
+            @Override
+            public ViewHolder onCreateViewHolder(ViewGroup parent, int position) {
+                return new ViewHolder(getLayoutInflater().inflate(R.layout.list_item, parent, false));
+            }
+
+            @Override
+            public void onBindViewHolder(ViewHolder viewHolder, int position) {
+                viewHolder.text1.setText("Bacon");
+                viewHolder.text2.setText("Bacon ipsum dolor amet pork belly meatball kevin spare ribs. Frankfurter swine corned beef meatloaf, strip steak.");
+            }
+
+            @Override
+            public int getItemCount() {
+                return 30;
+            }
+        });
+    }
+
+    private static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView text1;
+        TextView text2;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            text1 = (TextView) itemView.findViewById(android.R.id.text1);
+            text2 = (TextView) itemView.findViewById(android.R.id.text2);
+        }
     }
 }
