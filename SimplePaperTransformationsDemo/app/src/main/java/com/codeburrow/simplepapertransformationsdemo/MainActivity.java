5ba0749b38cee705fcff1cc2353c1209d9930ec7
George Spiridakis <spiridakis.grg@gmail.com>
2016-08-20 15:19:39 +0300

    Start the circular reveal animation.

--- SimplePaperTransformationsDemo/app/src/main/java/com/codeburrow/simplepapertransformationsdemo/MainActivity.java
+++ SimplePaperTransformationsDemo/app/src/main/java/com/codeburrow/simplepapertransformationsdemo/MainActivity.java
@@ -1,11 +1,15 @@
 package com.codeburrow.simplepapertransformationsdemo;
 
+import android.animation.Animator;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.transition.TransitionManager;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
@@ -45,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private static class ViewHolder extends RecyclerView.ViewHolder {
+    private static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         TextView text1;
         TextView text2;
@@ -56,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
             super(itemView);
             text1 = (TextView) itemView.findViewById(android.R.id.text1);
             text2 = (TextView) itemView.findViewById(android.R.id.text2);
+            itemView.setOnClickListener(this);
 
             if (green == 0)
                 green = itemView.getContext().getResources().getColor(R.color.green);
@@ -63,5 +68,44 @@ public class MainActivity extends AppCompatActivity {
                 white = itemView.getContext().getResources().getColor(R.color.background_material_light);
         }
 
+        @Override
+        public void onClick(View view) {
+            boolean isVeggie = view.getBackground() != null && ((ColorDrawable) view.getBackground()).getColor() == green;
+
+            TransitionManager.beginDelayedTransition((ViewGroup) view);
+            int finalRadius = Math.max(view.getWidth(), view.getHeight()) / 2;
+
+            if (isVeggie) {
+                text1.setText(baconTitle);
+                text2.setText(baconText);
+                view.setBackgroundColor(white);
+            } else {
+                /*
+                 * The signature of the createCircularReveal method:
+                 *
+                 * ViewAnimationUtils.createCircularReveal(
+                 *      View view,          // the view we' re going to operate on
+                 *      int centerX,        // a center X
+                 *      int centerY,        // and a center Y to start the transition
+                 *      float startRadius,  // a start radius
+                 *      float endRadius     // and an end radius
+                 * )
+                 *
+                 *  1. First we need to find the center point of our reveal:
+                 *          we can do that by taking the item's width and height
+                 *          and dividing in half.
+                 *  2. The reveal should start from nothing, so our starting radius is 0.
+                 *  3. The ending radius is half of the diameter:
+                 *          (fit our item to a circle)
+                 *          the diameter is a line segment that passes through the center
+                 *          and whose the end points lie on the circle.
+                 */
+                Animator anim = ViewAnimationUtils.createCircularReveal(view, view.getWidth() / 2, view.getHeight() / 2, 0, finalRadius);
+                text1.setText(veggieTitle);
+                text2.setText(veggieText);
+                view.setBackgroundColor(green);
+                anim.start();
+            }
+        }
     }
 }
