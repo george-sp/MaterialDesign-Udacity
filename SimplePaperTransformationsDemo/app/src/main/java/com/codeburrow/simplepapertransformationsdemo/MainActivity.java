68113a6e0107477adfb9cba471a60b19c9c4d60f
George Spiridakis <spiridakis.grg@gmail.com>
2016-08-20 14:02:24 +0300

    Configure the Toolbar, by setting navigation icon and title.

--- SimplePaperTransformationsDemo/app/src/main/java/com/codeburrow/simplepapertransformationsdemo/MainActivity.java
+++ SimplePaperTransformationsDemo/app/src/main/java/com/codeburrow/simplepapertransformationsdemo/MainActivity.java
@@ -1,13 +1,18 @@
 package com.codeburrow.simplepapertransformationsdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ((CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar_layout)).setTitle(getString(R.string.app_name));
     }
 }
