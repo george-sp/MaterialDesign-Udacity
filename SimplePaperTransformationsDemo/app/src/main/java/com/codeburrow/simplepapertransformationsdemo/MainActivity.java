d2592fed43c31123c800ea1d681814405d9c48df
George Spiridakis <spiridakis.grg@gmail.com>
2016-08-20 14:32:31 +0300

    Set up the RecyclerView in MainActivity.

--- SimplePaperTransformationsDemo/app/src/main/java/com/codeburrow/simplepapertransformationsdemo/MainActivity.java
+++ SimplePaperTransformationsDemo/app/src/main/java/com/codeburrow/simplepapertransformationsdemo/MainActivity.java
@@ -3,10 +3,20 @@ package com.codeburrow.simplepapertransformationsdemo;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    static String baconTitle = "Bacon";
+    static String baconText = "Bacon ipsum dolor amet pork belly meatball kevin spare ribs. Frankfurter swine corned beef meatloaf, strip steak.";
+    static String veggieTitle = "Veggie";
+    static String veggieText = "Veggies es bonus vobis, proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth tatsoi tomatillo melon azuki bean garlic.";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +24,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ((CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar_layout)).setTitle(getString(R.string.app_name));
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new RecyclerView.Adapter<ViewHolder>() {
+            @Override
+            public ViewHolder onCreateViewHolder(ViewGroup parent, int position) {
+                return new ViewHolder(getLayoutInflater().inflate(R.layout.recycler_view_list_item, parent, false));
+            }
+
+            @Override
+            public void onBindViewHolder(ViewHolder viewHolder, int position) {
+                viewHolder.text1.setText(baconTitle);
+                viewHolder.text2.setText(baconText);
+            }
+
+            @Override
+            public int getItemCount() {
+                return 10;
+            }
+        });
+    }
+
+    private static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView text1;
+        TextView text2;
+        static int green;
+        static int white;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            text1 = (TextView) itemView.findViewById(android.R.id.text1);
+            text2 = (TextView) itemView.findViewById(android.R.id.text2);
+
+            if (green == 0)
+                green = itemView.getContext().getResources().getColor(R.color.green);
+            if (white == 0)
+                white = itemView.getContext().getResources().getColor(R.color.background_material_light);
+        }
+
     }
 }
