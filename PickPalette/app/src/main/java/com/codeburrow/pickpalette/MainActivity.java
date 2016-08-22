81a33fd8049ce0743833afc5e9eb4a992b150ee2
George Spiridakis <spiridakis.grg@gmail.com>
2016-08-22 17:29:43 +0300

    Access UI elements using ButterKnife library.

--- PickPalette/app/src/main/java/com/codeburrow/pickpalette/MainActivity.java
+++ PickPalette/app/src/main/java/com/codeburrow/pickpalette/MainActivity.java
@@ -1,13 +1,26 @@
 package com.codeburrow.pickpalette;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    @InjectView(R.id.tool_bar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.inject(this);
+
+        toolbar.setTitle(getString(R.string.app_name));
     }
 }
