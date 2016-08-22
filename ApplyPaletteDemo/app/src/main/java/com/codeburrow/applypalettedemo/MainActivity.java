4477aada35436b55cc4cfc0b340ebb84b4498955
George Spiridakis <spiridakis.grg@gmail.com>
2016-08-22 12:43:27 +0300

    Use a RecyclerView to fill in the blank space of the layout.

--- ApplyPaletteDemo/app/src/main/java/com/codeburrow/applypalettedemo/MainActivity.java
+++ ApplyPaletteDemo/app/src/main/java/com/codeburrow/applypalettedemo/MainActivity.java
@@ -3,6 +3,11 @@ package com.codeburrow.applypalettedemo;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +19,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ((CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar_layout)).setTitle(getString(R.string.app_name));
+        RecyclerView rv = (RecyclerView) findViewById(R.id.recyclerview);
+        rv.setLayoutManager(new LinearLayoutManager(this));
+        rv.setAdapter(new RecyclerView.Adapter<ViewHolder>() {
+            @Override
+            public ViewHolder onCreateViewHolder(ViewGroup parent, int position) {
+                return new ViewHolder(getLayoutInflater().inflate(R.layout.recycler_view_list_item, parent, false));
+            }
+
+            @Override
+            public void onBindViewHolder(ViewHolder viewHolder, int position) {
+                viewHolder.text1.setText(R.string.bacon_title);
+                viewHolder.text2.setText(R.string.bacon_body);
+            }
+
+            @Override
+            public int getItemCount() {
+                return 30;
+            }
+        });
+    }
+
+    private static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private static final String LOG_TAG = ViewHolder.class.getSimpleName();
+
+        TextView text1;
+        TextView text2;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            text1 = (TextView) itemView.findViewById(android.R.id.text1);
+            text2 = (TextView) itemView.findViewById(android.R.id.text2);
+        }
     }
 }
