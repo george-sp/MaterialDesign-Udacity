b0331541abba0ed5c62019b5f83ab2afc8c3e3ab
George Spiridakis <spiridakis.grg@gmail.com>
2016-08-22 18:37:50 +0300

    Pick a photo from your gallery.

--- PickPalette/app/src/main/java/com/codeburrow/pickpalette/PickerFragment.java
+++ PickPalette/app/src/main/java/com/codeburrow/pickpalette/PickerFragment.java
@@ -1,22 +1,36 @@
 package com.codeburrow.pickpalette;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.Button;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 public class PickerFragment extends DialogFragment {
 
     private static final String LOG_TAG = PickerFragment.class.getSimpleName();
 
+    private static final int PICK_PHOTO = 100;
+
+    @InjectView(R.id.pickImage)
+    Button pickImage;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_picker, container, false);
+        ButterKnife.inject(this, view);
         getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
         return view;
     }
@@ -24,5 +38,27 @@ public class PickerFragment extends DialogFragment {
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
+    }
+
+    @OnClick(R.id.pickImage)
+    public void pickImage(View view) {
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, PICK_PHOTO);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case PICK_PHOTO:
+                if (resultCode == Activity.RESULT_OK) {
+                    Log.e(LOG_TAG, "Picked a photo.");
+                    Uri selectedImage = data.getData();
+                    getDialog().dismiss();
+                }
+                break;
+        }
     }
 }
