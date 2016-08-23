69d0896cec172016aeb9177e59712f35aa904568
George Spiridakis <spiridakis.grg@gmail.com>
2016-08-23 20:06:58 +0300

    Create the MainActivityFragment.

---
+++ TypographicalDemo/app/src/main/java/com/codeburrow/typographicaldemo/MainActivityFragment.java
@@ -0,0 +1,23 @@
+package com.codeburrow.typographicaldemo;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivityFragment extends Fragment {
+
+    private static final String LOG_TAG = MainActivityFragment.class.getSimpleName();
+
+    /* Empty constructor per the documentation. */
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+}
