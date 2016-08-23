00dd642d8462daee9c3af54cf61b34e32f867367
George Spiridakis <spiridakis.grg@gmail.com>
2016-08-24 00:10:03 +0300

    Find views by id using ButterKnife library.

--- TypographicalDemo/app/src/main/java/com/codeburrow/typographicaldemo/MainActivityFragment.java
+++ TypographicalDemo/app/src/main/java/com/codeburrow/typographicaldemo/MainActivityFragment.java
@@ -5,10 +5,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 public class MainActivityFragment extends Fragment {
 
     private static final String LOG_TAG = MainActivityFragment.class.getSimpleName();
+
+    @InjectView(R.id.display4)
+    TextView display4View;
+    @InjectView(R.id.headline)
+    TextView headlineView;
 
     /* Empty constructor per the documentation. */
     public MainActivityFragment() {
@@ -18,6 +27,15 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ButterKnife.inject(this, view);
+        /*
+            display4View = (TextView)getActivity().findViewById(R.id.display4);
+            headlineView = (TextView)getActivity().findViewById(R.id.headline);
+         */
+        display4View.setTypeface(courgette);
+        headlineView.setTypeface(courgette);
+
         return view;
     }
 }
