4ddac6535530028f396386cb32be2c184fa98dad
George Spiridakis <spiridakis.grg@gmail.com>
2016-08-27 13:51:32 +0300

    Instantiate the Interpolator.

--- InterpolationDemo/README.md
+++ InterpolationDemo/README.md
@@ -1,3 +1,15 @@
 ## InterpolationDemo
 ----------------------------------
 This app is an example of interpolation.
+
+==================================
+
+**Details:**
+- The app has two spinners.
+	- The first one has a list of a bunch of different interpolators like _accelerate_, _decelerate_, _bounce_, _decelerate_, _linear_, _overshoot_.
+	- And the second spinner has different timings, from 100 milliseconds all the way up to 3,000 milliseconds or three seconds, _so that you can see how fast or how slow things work_.
+
+**In Code:**
+- Create a new instance of the interpolator.
+	- Because it's different based off of what the name is, we actually use the Java **`Class.forName(String className)`** to get the class and then instantiate a new instance.
+	- But if you were using this in your own app, you would call, **`new BounceInterpolator()`** directly.
--- InterpolationDemo/app/src/main/java/com/codeburrow/interpolationdemo/MainActivity.java
+++ InterpolationDemo/app/src/main/java/com/codeburrow/interpolationdemo/MainActivity.java
@@ -2,6 +2,8 @@ package com.codeburrow.interpolationdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.view.animation.Interpolator;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -21,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
     TextView textView;
 
     private int duration;
+    private static final String PACKAGE = "android.view.animation.";
+    private static final String PACKAGE_V4 = "android.support.v4.view.animation.";
+    private Interpolator interpolator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +53,37 @@ public class MainActivity extends AppCompatActivity {
             default:
                 duration = 300;
                 break;
+        }
+        // Kick off transition
+        onItemSelected(interpolatorSpinner, position);
+    }
+
+    String findFullInterpolatorPath(String className) {
+        if (className.equals("FastOutLinearInInterpolator") || className.equals("FastOutSlowInInterpolator") || className.equals("LinearOutSlowInInterpolator"))
+            return PACKAGE_V4 + className;
+        else if (className.startsWith("-"))
+            return null;
+        else return PACKAGE + className;
+    }
+
+    @OnItemSelected({R.id.interpolator_spinner})
+    void onItemSelected(Spinner spinner, int position) {
+        String interpolatorName = (String) spinner.getAdapter().getItem(position);
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        textView.setTranslationY(metrics.heightPixels);
+
+        try {
+            String path = findFullInterpolatorPath(interpolatorName);
+            if (path == null) return;
+            /*
+             * Create an instance of Interpolator:
+             * Because it's different based off of what the name is, we actually use the Java
+             * class forName to get the class and then instantiate a new instance.
+             */
+            interpolator = (Interpolator) Class.forName(path).newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
