0302f1d4c749ed288be266ad770447143f41e969
George Spiridakis <spiridakis.grg@gmail.com>
2016-08-28 19:34:41 +0300

    Apply CoordinatedMotion on Multiple CardView Elements.

--- CoordinatedMotionDemo/README.md
+++ CoordinatedMotionDemo/README.md
@@ -1,3 +1,16 @@
 ## CoordinatedMotionDemo
 ----------------------------------
 In this application more interesting movement is created by animating multiple objects, or even the children of an item as the parent itself moves.
+
+==================================
+
+#### Coordinated Motion - Multiple Elements
+In this example, the cards move onto screen at different speeds, helping to convey that
+they are separate items.
+
+When doing this, it's key that you do so in a coordinated matter, here we are achieving coordinated motion by using a consistent direction and timing.
+
+All cards move in from the bottom of the screen, from different distances, which causes the closing up effect.
+
+Because they use the same duration and interpolator, the acceleration and deceleration will link
+them perceptively,thanks to the gestalt laws of grouping that we looked at earlier.
--- CoordinatedMotionDemo/app/src/main/java/com/codeburrow/coordinatedmotiondemo/MultipleElements.java
+++ CoordinatedMotionDemo/app/src/main/java/com/codeburrow/coordinatedmotiondemo/MultipleElements.java
@@ -1,7 +1,11 @@
 package com.codeburrow.coordinatedmotiondemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
+import android.view.animation.Interpolator;
 
 public class MultipleElements extends AppCompatActivity {
 
@@ -9,5 +13,34 @@ public class MultipleElements extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multiple_element);
+    }
+
+    private void animateViewsIn() {
+        ViewGroup root = (ViewGroup) findViewById(R.id.root);
+        int count = root.getChildCount();
+        float offset = getResources().getDimensionPixelSize(R.dimen.offset_y);
+        Interpolator interpolator = AnimationUtils.loadInterpolator(this, android.R.interpolator.linear_out_slow_in);
+
+        // Loop over the children setting an increasing translation y but the same animation
+        // duration + interpolation.
+        for (int i = 0; i < count; i++) {
+            View view = root.getChildAt(i);
+            view.setVisibility(View.VISIBLE);
+            view.setTranslationY(offset);
+            view.setAlpha(0.85f);
+            // Then animate back to natural position.
+            view.animate()
+                    .translationY(0f)
+                    .alpha(1f)
+                    .setInterpolator(interpolator)
+                    .setDuration(1000L)
+                    .start();
+            // Increase the offset distance for the next view
+            offset *= 1.5f;
+        }
+    }
+
+    public void containerClick(View view) {
+        animateViewsIn();
     }
 }
