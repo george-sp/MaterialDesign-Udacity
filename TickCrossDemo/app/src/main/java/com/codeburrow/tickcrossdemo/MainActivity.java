efbfd0cbd276041941d854069d1e07e62664339b
George Spiridakis <spiridakis.grg@gmail.com>
2016-08-30 11:55:39 +0300

    Start the animation when the root view of the layout is clicked.

--- TickCrossDemo/app/src/main/java/com/codeburrow/tickcrossdemo/MainActivity.java
+++ TickCrossDemo/app/src/main/java/com/codeburrow/tickcrossdemo/MainActivity.java
@@ -1,13 +1,31 @@
 package com.codeburrow.tickcrossdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.drawable.AnimatedVectorDrawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView tickCross;
+    private AnimatedVectorDrawable tickToCross;
+    private AnimatedVectorDrawable crossToTick;
+    private boolean tick = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tickCross = (ImageView) findViewById(R.id.tick_cross);
+        tickToCross = (AnimatedVectorDrawable) getDrawable(R.drawable.avd_tick_to_cross);
+        crossToTick = (AnimatedVectorDrawable) getDrawable(R.drawable.avd_cross_to_tick);
+    }
+
+    public void animate(View view) {
+        AnimatedVectorDrawable drawable = tick ? tickToCross : crossToTick;
+        tickCross.setImageDrawable(drawable);
+        drawable.start();
+        tick = !tick;
     }
 }
