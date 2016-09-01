80fdbb5d0b61c36ad59ea0a61e9c027b7e29712f
George Spiridakis <spiridakis.grg@gmail.com>
2016-09-01 21:35:52 +0300

    Create a custom RecyclerView.ItemDecoration inner class.

--- AspectRatioDemo/app/src/main/java/com/codeburrow/aspectratiodemo/MainActivity.java
+++ AspectRatioDemo/app/src/main/java/com/codeburrow/aspectratiodemo/MainActivity.java
@@ -1,12 +1,15 @@
 package com.codeburrow.aspectratiodemo;
 
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.ProgressBar;
 
 import butterknife.Bind;
+import butterknife.BindDimen;
 import butterknife.BindInt;
 import butterknife.ButterKnife;
 
@@ -20,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     ProgressBar empty;
     @BindInt(R.integer.photo_grid_columns)
     int columns;
+    @BindDimen(R.dimen.grid_item_spacing)
+    int gridSpacing;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,6 +71,32 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         grid.setLayoutManager(gridLayoutManager);
+        grid.addItemDecoration(new GridMarginDecoration(gridSpacing));
         grid.setHasFixedSize(true);
+    }
+
+    /**
+     * An ItemDecoration allows the application to add a special drawing and layout offset
+     * to specific item views from the adapter's data set.
+     * This can be useful for drawing dividers between items, highlights, visual grouping boundaries and more.
+     * All ItemDecorations are drawn in the order they were added,
+     * before the item views (in onDraw() and after the items in onDrawOver(Canvas, RecyclerView, RecyclerView.State)).
+     */
+    private static class GridMarginDecoration extends RecyclerView.ItemDecoration {
+
+        private int space;
+
+        public GridMarginDecoration(int space) {
+            this.space = space;
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view,
+                                   RecyclerView parent, RecyclerView.State state) {
+            outRect.left = space;
+            outRect.top = space;
+            outRect.right = space;
+            outRect.bottom = space;
+        }
     }
 }
